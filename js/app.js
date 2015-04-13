c44eefda8a680474b23fe1cdba4d5d87d5c484f8
The_Con <j_rafe@hotmail.fr>
2015-04-13 17:49:33 +0200

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/* DTC Calendar :
+    *** Vue par mois (à l'itialisation le mois actuel)
+    *** Vue par année
+    *** Spécification du jour actuel, des jours fériés
+*/
+(function() {
+    var app = angular.module('App', ['DtcCalendarApp']);
+    
+})();
